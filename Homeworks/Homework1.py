7fc11cf74e2771714e576784079fa175fd138fb6
Asli-Yilmaz <75321471+Asli-Yilmaz@users.noreply.github.com>
2021-02-19 22:48:22 +0300

    Add files via upload

--- Homeworks/Homework1.py
+++ Homeworks/Homework1.py
@@ -1,1 +1,20 @@
-
+import random as rnd
+matrix=[]
+for i in range(3):
+    number=0
+    row=[]
+    while number!=3:
+        n=rnd.randint(0,100)
+        if(n<2):continue
+        elif(n==2 or n==3 or n==5 or n==7):
+            row.append(n)
+            number+=1
+        elif(n%2==0 or n%3==0 or n%5==0 or n%7==0):continue       
+        else:
+            row.append(n)
+            number+=1
+    matrix.append(row)
+for i in range(3):
+    for j in range(3):
+        print(matrix[i][j],end=" ")
+    print()
